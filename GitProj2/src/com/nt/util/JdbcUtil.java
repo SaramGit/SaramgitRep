097ed04564bb5b453224d7fb4924952900bafcf2
TL <TL@gmail.com>
2019-04-01 00:10:43 +0530

    Initialcommit2by tl

---
+++ GitProj2/src/com/nt/util/JdbcUtil.java
@@ -0,0 +1,11 @@
+package com.nt.util;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+
+public class JdbcUtil {
+
+	   public static Connection getConnection(String url,String uname,String pwd) throws Exception {
+		   return DriverManager.getConnection("jdbc:oracle:thin:@localhost:1521:xe", "system", "12345");
+	   }
+}
