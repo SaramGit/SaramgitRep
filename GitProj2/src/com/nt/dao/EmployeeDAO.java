e28bcd89b35e606d34ca7d5b34c2310fe6379717
Dev2 <Dev2@gmail.com>
2019-04-01 10:43:05 +0530

    Empmaxsal finding by Dev2

--- GitProj2/src/com/nt/dao/EmployeeDAO.java
+++ GitProj2/src/com/nt/dao/EmployeeDAO.java
@@ -24,4 +24,20 @@ public class EmployeeDAO {
 	    	count=rs.getInt(1);
 	    	return count;	
 	    }
+	    public int getMaxSal() throws Exception{
+	    	Connection con=null;
+	    	PreparedStatement ps=null;
+	    	ResultSet rs=null;
+	    	int salary=0;
+	    	//get connetion
+	    	con=JdbcUtil.getConnection("jdbc:oracle:thin:@localhost:1521:xe","system","12345");
+	        //create the prepared statement object
+	    	ps=con.prepareStatement("SELECT Max(Sal) FROM EMP");
+	    	//execute the query
+	    	rs=ps.executeQuery();
+	    	//process the result
+	    	rs.next();
+	    	salary=rs.getInt(1);
+	    	return salary;
+	    }
 }
--- GitProj2/src/com/nt/test/JdbcTest.java
+++ GitProj2/src/com/nt/test/JdbcTest.java
@@ -9,11 +9,14 @@ public class JdbcTest {
 	public static void main(String[] args) {
 		EmployeeDAO dao=null;
 		int count=0;
+		int salary=0;
 		//create the employee dao class object
 		dao=new EmployeeDAO();
 		try {
 			count=dao.getEmpCount();
 			System.out.println("Employee Count is: "+count);
+			salary=dao.getMaxSal();
+			System.out.println("Employee Maxsalary is: "+salary);
 		}catch(SQLException se) {
 			se.printStackTrace();
 		}catch(Exception e) {
