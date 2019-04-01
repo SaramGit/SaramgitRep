b9263aa768b3d0535a3eb877c420e9c5d4fe63ee
TL <TL@gmail.com>
2019-04-01 12:28:09 +0530

    Dev1 in branch done his service class work

--- GitProj2/src/com/nt/service/EmployeeMgmtService.java
+++ GitProj2/src/com/nt/service/EmployeeMgmtService.java
@@ -1,5 +1,15 @@
 package com.nt.service;
 
+import com.nt.dao.EmployeeDAO;
+
 public class EmployeeMgmtService {
 
+	public int getEmpCount() throws Exception{
+		EmployeeDAO dao=null;
+		int count=0;
+		//use DAO
+		dao=new EmployeeDAO();
+		count=dao.getEmpCount();
+		return count+10;
+	}
 }
--- GitProj2/src/com/nt/test/JdbcTest.java
+++ GitProj2/src/com/nt/test/JdbcTest.java
@@ -3,20 +3,27 @@ package com.nt.test;
 import java.sql.SQLException;
 
 import com.nt.dao.EmployeeDAO;
+import com.nt.service.EmployeeMgmtService;
 
 public class JdbcTest {
 
 	public static void main(String[] args) {
 		EmployeeDAO dao=null;
+		EmployeeMgmtService service=null;
 		int count=0;
 		int salary=0;
 		//create the employee dao class object
 		dao=new EmployeeDAO();
+		//create the employee service class object
+		service=new EmployeeMgmtService();
 		try {
-			count=dao.getEmpCount();
-			System.out.println("Employee Count is: "+count);
+			//count=dao.getEmpCount();
+			//System.out.println("Employee Count is: "+count);
 			salary=dao.getMaxSal();
 			System.out.println("Employee Maxsalary is: "+salary);
+			//service class count
+			count=service.getEmpCount();
+			System.out.println("Employee Count is: "+count);
 		}catch(SQLException se) {
 			se.printStackTrace();
 		}catch(Exception e) {
